2b78c348fa3218a6ca9ae8eb3d5323297f7c1d25
Michael Ruoss <mruoss.git@ufirstgroup.com>
2010-04-18 11:36:40 +0000

    More development on the 7.x branch. Migrating the module to the new version of Drupal.

--- js/slider.js
+++ js/slider.js
@@ -1,5 +1,14 @@
-function changeHandle(e,ui) {
-  var id = ui.handle.attr('id').split('_handle').join('');
-  $("#"+id+"_value").val(ui.value);
-  $("#"+id+"_nr").text(ui.value+"%");
+Sliders = {}
+
+Sliders.changeHandle = function(e,ui) {
+  var id = jQuery(ui.handle).parents('div.slider-widget-container').attr('id');
+  if (typeof(ui.values) != 'undefined') {
+    jQuery.each(ui.values, function(i,val) {
+      jQuery("#"+id+"_value_"+i).val(val);
+      jQuery("#"+id+"_nr_"+i).text(val+"%");
+    });
+  } else {
+    jQuery("#"+id+"_value_0").val(ui.value);
+    jQuery("#"+id+"_nr_0").text(ui.value+"%");
+  }
 }
--- theme/slider-range.tpl.php
+++ theme/slider-range.tpl.php
@@ -8,35 +8,31 @@
  * Available variables:
  * - $element: The element array containing #id, #name,...
  */
-
 ?>
 <div>
   <strong><?php echo $element["#title"];?>:</strong>
 </div>
-<div style="float:left">
-  <div id='<?php echo $element["#id"]; ?>_slider' class='ui-slider-1'></div>
+<div id="<?php echo $element["#id"]; ?>" class="slider-widget-container">
+  <div style="float:left">
+    <div id='<?php echo $element["#id"]; ?>_slider' class='ui-slider-1'></div>
+  </div>
+  <span style="margin-left: 15px;">[<span id='<?php echo $element["#id"]; ?>_nr_0'><?php echo $element["#default"][0]; ?>%</span>,<span id='<?php echo $element["#id"]; ?>_nr_1'><?php echo $element["#default"][1]; ?>%</span>]</span>
 </div>
-<span style="margin-left: 5px;">[<span id='<?php echo $element["#id"]; ?>_lower_nr'><?php echo $element["#default_value"][0]; ?>%</span>,<span id='<?php echo $element["#id"]; ?>_upper_nr'><?php echo $element["#default_value"][1]; ?>%</span>]</span>
 <div style="clear:both;"></div>
 <script type="text/javascript">
 <!--
-  $('#<?php echo $element["#id"];?>_slider').slider({
-    slide: changeHandle,
-    steps: <?php echo $element["#steps"]; ?>,
-    handles: [
-      {
-        start: <?php echo $element["#default_value"][0]; ?>,
-        id: '<?php echo $element["#id"];?>_handle_lower'
-      },
-      {
-        start: <?php echo $element["#default_value"][1]; ?>,
-        id: '<?php echo $element["#id"];?>_handle_upper'
-      }
+  jQuery('#<?php echo $element["#id"];?>_slider').slider({
+    slide: Sliders.changeHandle,
+    step: 100/<?php echo $element["#steps"]; ?>,
+    values: [
+      <?php echo $element["#default"][0]; ?>,
+      <?php echo $element["#default"][1]; ?>,
+      
     ],
     range: true
 });
 //-->
 </script>
 <div class="description" style="margin-bottom:10px;"><?php echo $element["#description"];?></div>
-<input type="hidden" name='<?php echo $element["#name"]; ?>[]' id='<?php echo $element["#id"];?>_lower_value' value='<?php echo $element["#default_value"][0]; ?>' />
-<input type="hidden" name='<?php echo $element["#name"]; ?>[]' id='<?php echo $element["#id"];?>_upper_value' value='<?php echo $element["#default_value"][1]; ?>' />
+<input type="hidden" name='<?php echo $element["#name"]; ?>[]' id='<?php echo $element["#id"];?>_value_0' value='<?php echo $element["#default"][0]; ?>' />
+<input type="hidden" name='<?php echo $element["#name"]; ?>[]' id='<?php echo $element["#id"];?>_value_1' value='<?php echo $element["#default"][1]; ?>' />
